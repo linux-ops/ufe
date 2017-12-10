655a305fe0f15806b9c2f01bddbdc53872c393c3
王超 <email_wangchao@163.com>
2017-12-10 20:10:42 +0800

    delete body str

--- add_upstream.py
+++ add_upstream.py
@@ -6,7 +6,6 @@ utree_list=open(filename,'r')
 for f in  utree_list:
 
  f = f.strip('\n')
- body = {"m_name":f,"m_loadbalanceStrategyName": "round","m_health_check": {"interval": "3000", "check_keepalive_requests": "5", "timeout": "3000", "rise": "1", "enable_health_check":  'true', "check_http_send": "GET /healthCheck.jsp  HTTP/1.0\\r\\n\\r\\n", "default_down": 'false', "fall": "3", "check_http_expect_alive": ["http_2xx", "http_3xx"], "type": "http", "port": "80"}}
  rst = json.dumps(body,indent=4, separators=(',', ': '))
  print rst+','
 utree_list.close()
